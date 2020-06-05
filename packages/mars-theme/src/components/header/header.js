ca0128aecbe43c0b1ccca4072444f16984ce2c4d
Владимир Лучик <volodymyr641@gmail.com>
2020-06-05 21:25:04 +0300

    header_&_mobile

--- packages/mars-theme/src/components/header/header.js
+++ packages/mars-theme/src/components/header/header.js
@@ -2,16 +2,25 @@ import React from 'react';
 import {connect,styled} from 'frontity';
 import group_menu from '../header/img/group_menu.svg';
 import logo from '../header/img/logo.svg';
+import CrossImg from '../header/img/cross.svg'
 import Button from '../button/Button';
 
 import {
-    FullHead,
     HeadBlock,
     Header,
     Mobilemenu,
     Logo,
     MobileButton,
     GetButton,
+    PopUpWindow,
+    MobileBody,
+    MobileOption,
+    LogoBlock,
+    MobileLogo,
+    MobileTopMenu,
+    MobileGetButton,
+    CrossBlock,
+    Cross,
 } from './styles';
 
 const header = ({}) => {
@@ -65,6 +74,67 @@ const header = ({}) => {
             </Button>
         </GetButton>
 </Header>
+
+<PopUpWindow>
+    <MobileBody>
+    <MobileOption>
+    <LogoBlock>
+    <MobileLogo>
+    <a href = "#"><img src = {logo}alt = "Logo" /></a>
+    </MobileLogo>
+    </LogoBlock>
+    <MobileTopMenu>
+    <ul>
+    <li>
+    <a href = {'#'} > ABOUT US </a>
+    <ul className = 'sub-menu'>
+    <li><a href = {'#'}> Profile </a></li>
+    <li><a href = {'#'}> Expertise </a></li>
+    <li>< a href = {'#'}> Testimonials </a></li>
+    <li><a href = {'#'}> Team </a></li>
+    <li><a href = {'#'}> Faq </a></li>
+    <li><a href = {'#'}> Referral Program </a></li>
+    </ul>
+    </li>
+    <li>
+    <a href = {'#'}><span> SERVICES </span></a>
+    <ul className = 'sub-menu'>
+    <li><a href = {'#'}> R&D Center </a></li>
+    <li><a href = {'#'}> IT Staff <br/> Augmentation </a></li>
+    </ul>
+    </li>
+    <li>
+    <a href = {'#'}> CASE STUDIES </a><ul className = 'sub-menu'>
+        <li>
+            <a href = {'#'} > Linux, Embedded < br/> And Iot </a>
+        </li>
+        <li>
+            <a href = {'#'}> IT Staff <br/> Augmentation </a>
+        </li>
+    </ul>
+    </li>
+    <li>
+        <a href = {'#'}> CAREERS </a>
+        </li>
+    <li>
+        <a href = {'#'}> BLOG </a>
+        </li>
+    <li><a href = {'#'}> CONTACT US </a></li>
+    </ul>
+    </MobileTopMenu>
+    </MobileOption>
+    <MobileGetButton>
+    <Button>
+    <a href = {'#'}> GET A FREE QUOTE </a>
+    </Button>
+    </MobileGetButton>
+    </MobileBody>
+    <CrossBlock>
+    <Cross>
+    <a href = "#"><img src = {CrossImg}alt = "cross"/></a>
+    </Cross>
+    </CrossBlock>
+    </PopUpWindow>
 </HeadBlock>
     );
         } 
--- packages/mars-theme/src/components/header/styles.js
+++ packages/mars-theme/src/components/header/styles.js
@@ -148,4 +148,105 @@ export const GetButton = styled.div `
 }
 `;
 
+export const PopUpWindow = styled.div `
+    margin-top: 5px;
+    display: flex;
+    justify-content: space-around;
+    overflow: scroll;
+    background-color:#FFFFFF;
+    @media(min-width: 1100px) {
+        display: none;
+    }
+
+`;
+
+export const MobileBody = styled.div `
+    margin-left: 20px;
+    padding-right: 10px;
+    width: 90%;
+    height: 380px;
+
+`;
+
+export const MobileOption = styled.div `
+    text-align: center;
+    height: 320px;     
+`;
+
+export const LogoBlock = styled.div `
+    margin-top: 10px;
+    margin-bottom: 11px;
+    display: flex;
+    justify-content: center;
+
+`;
+
+
+export const MobileLogo = styled.div `
+    padding-left: 50px;
+
+`;
+
+export const MobileTopMenu = styled.div `
+    padding:0;
+    ul li a {
+        text-decoration: none;
+        color: #222222;
+        font-size: 14px;
+        line-height: 17px;
+        font-style: normal;
+        font-weight: 500;
+    }
+    a {
+        text-decoration: none;
+    }
+    li span {
+        color: #FF8D23;
+    }
+    li {
+        height: 45px;
+        list-style-type: none;
+    }
+        .sub-menu li {
+            margin-left: -30px;
+            margin-right: 10px;
+            margin-top: 10px;
+            margin-bottom: 5px;
+        }
+        .sub-menu {
+            display: none;
+            position: absolute;
+            margin-left: -10px;
+            width: 157px;
+            background: #fff;
+            box-shadow: 2px 4px 4px rgba(0, 0, 0, .25);
+            z-index: 5;
+            text-align: left;
+        }
+
+    font-weight: 500;
+    @media(min-width: 1100px) {
+            display: none;
+    }
+`;
+
+export const MobileGetButton = styled.div `
+    text-align: center;
+    padding-left: 40px;
+`;
+
+export const CrossBlock = styled.div `
+    width: 10%;
+    margin-top: 10px;
+    margin-right: 10px;
+`;
+
+export const Cross = styled.div `
+    @media(min-width: 1100px) {
+                display: none;
+            }
+
+`;
+
+
 
