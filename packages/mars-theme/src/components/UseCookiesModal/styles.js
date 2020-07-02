d9fc0deb85d295eeda885b924cb7bb6e05d8ef51
Sasha <sachazaets@ukr.net>
2020-07-02 21:05:39 +0300

    add modal

---
+++ packages/mars-theme/src/components/UseCookiesModal/styles.js
@@ -0,0 +1,10 @@
+import { styled } from 'frontity';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 50px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: black;
+`;
--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -15,6 +15,7 @@ import { Space, globalStyles, Main } from './globalStyles';
 import FeedbackForm from './FeedbackForm';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import Recaptcha from './Recaptcha';
+import UseCookiesModal from './UseCookiesModal';
 
 /**
  * Theme is the root React component of our theme. The one we will export
@@ -62,6 +63,9 @@ const Theme = ({ state, actions }) => {
           isOpen={state.customSettings.isSubscribeSend}
           handleClose={subscribeHandleClose}
         />
+
+        <UseCookiesModal />
+        
         <Header scrollRef={formRef} />
         <Space />
 
