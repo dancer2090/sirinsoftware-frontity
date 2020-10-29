21f7876498fa203a4ae1b631c5d1fcf84d96a85c
yuriy-arttime <yuriy3304@gmail.com>
2020-10-29 10:44:44 +0300

    add robots and fix faq page

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -56,6 +56,8 @@ const Theme = ({ state, actions }) => {
     actions.theme.ipDetect();
   }, []);
 
+  const currUrl = state.router.link;
+
   return (
     <>
       <GoogleReCaptchaProvider reCaptchaKey={recaptchaKey}>
@@ -64,7 +66,14 @@ const Theme = ({ state, actions }) => {
         <Title />
         <Head>
           <meta name="description" content={state.frontity.description} />
-          <html lang="en" />
+            {
+              state.router.link.indexOf('/faq') !== -1 ?
+              (
+                <html lang="en" itemscope itemtype="https://schema.org/FAQPage" />
+              ) : (
+                <html lang="en" />
+              )
+            }
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         </Head>
 
